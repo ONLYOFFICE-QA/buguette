dd36c15db9c28d74a69864bc969a22ffd5afa299
Dmitriy Rotat <kvazilife@gmail.com>
2020-06-04 18:46:36 +0300

    Add new model - user

---
+++ src/app/models/user.ts
@@ -0,0 +1,21 @@
+
+export interface UserResponceData {
+  id: string;
+  name: string;
+  real_name:string;
+  email: string;
+}
+
+// this object describe bug
+export class User {
+  id: string;
+  name: string;
+  real_name: string;
+  email: string;
+  constructor(bugData: UserResponceData) {
+    this.id = bugData['id']
+    this.name = bugData['name']
+    this.real_name = bugData['real_name']
+    this.email = bugData['email']
+  }
+}
