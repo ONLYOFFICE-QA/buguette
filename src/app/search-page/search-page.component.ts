9c97bfa4d9dbf30a9f6eebb6d0f210eb55c0c95f
Dmitriy Rotat <kvazilife@gmail.com>
2020-06-26 18:41:17 +0300

    fix: searching user by full name

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -77,6 +77,9 @@ export class SearchPageComponent implements OnInit {
           result = true;
         }
       })
+      if (!result) {
+        result = user.real_name.toLowerCase().indexOf(filterValue) === 0
+      }
       return result;
     });
   }
