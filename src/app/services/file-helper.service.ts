e7e1ceac546c1b0bfcd14bd20f1f7f67196c792d
Dmitriy Rotat <kvazilife@gmail.com>
2020-08-07 13:23:02 +0300

    codestyle change

--- src/app/services/file-helper.service.ts
+++ src/app/services/file-helper.service.ts
@@ -8,26 +8,26 @@ export class FileHelperService {
   constructor() { }
 
   public base64ToBlob(b64Data: string, contentType = '', sliceSize = 512) {
-    b64Data = b64Data.replace(/\s/g, ''); //IE compatibility...
-    let byteCharacters = atob(b64Data);
-    let byteArrays = [];
+    b64Data = b64Data.replace(/\s/g, ''); // IE compatibility...
+    const byteCharacters = atob(b64Data);
+    const byteArrays = [];
     for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      let slice = byteCharacters.slice(offset, offset + sliceSize);
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
 
-      let byteNumbers = new Array(slice.length);
-      for (var i = 0; i < slice.length; i++) {
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
         byteNumbers[i] = slice.charCodeAt(i);
       }
-      let byteArray = new Uint8Array(byteNumbers);
+      const byteArray = new Uint8Array(byteNumbers);
       byteArrays.push(byteArray);
     }
     return new Blob(byteArrays, { type: contentType });
   }
 
   download_file_by_base64(data: string, type: string, name: string) {
-    var blob = this.base64ToBlob(data, type);
-    var link = document.createElement("a");
-    var url = window.URL.createObjectURL(blob);
+    const blob = this.base64ToBlob(data, type);
+    const link = document.createElement('a');
+    const url = window.URL.createObjectURL(blob);
     link.href = url;
     link.download = name;
     link.click();
