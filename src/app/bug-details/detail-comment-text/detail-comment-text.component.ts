d4fb82aa5a8412b423d5d9f94bba5c1e48a41ef6
Dmitriy Rotat <kvazilife@gmail.com>
2020-08-07 17:11:00 +0300

    refactoring for detail-comment-text

--- src/app/bug-details/detail-comment-text/detail-comment-text.component.ts
+++ src/app/bug-details/detail-comment-text/detail-comment-text.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 export interface CommentTextObjectsInterface {
-  commentType?: "push" | "generic-comment";
+  commentType?: 'push' | 'generic-comment';
   objects?: any[];
+}
+
+export interface CommitResultObjectInterface {
+  branch?: string;
+  link?: string;
+  message?: string;
+  author?: string;
 }
 
 @Component({
@@ -18,25 +25,25 @@ export class DetailCommentTextComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.text.indexOf('Commit pushed to') === 0) {
-      this.commentTextObjects.commentType = "push";
+      this.commentTextObjects.commentType = 'push';
       this.commentTextObjects.objects = this.parce_pushed_commit(this.text);
     } else {
-      this.commentTextObjects.commentType = "generic-comment";
+      this.commentTextObjects.commentType = 'generic-comment';
       this.commentTextObjects.objects = [this.text];
     }
   }
 
   parce_pushed_commit(text: string) {
-    let results = {};
-    results['branch'] = this.text.match(/Commit pushed to.(\S*)/)[1].trim();
-    results['link'] = this.text.match(/^https:\/\/github.com.*/gm)[0].trim();
+    const results: CommitResultObjectInterface = {};
+    results.branch = this.text.match(/Commit pushed to.(\S*)/)[1].trim();
+    results.link = this.text.match(/^https:\/\/github.com.*/gm)[0].trim();
     if (/Author:(.*)/.test(this.text)) {
-      results['author'] = this.text.match(/Author:(.*)/)[1].trim();
+      results.author = this.text.match(/Author:(.*)/)[1].trim();
     }
     if (/Message:(.*)/.test(this.text)) {
-      results['message'] = this.text.match(/Message:(.*)/)[1].trim();
+      results.message = this.text.match(/Message:(.*)/)[1].trim();
     } else {
-      results['message'] = this.text.match(/\n.*(?!.*\n)/)[0].trim();
+      results.message = this.text.match(/\n.*(?!.*\n)/)[0].trim();
     }
     return [results];
   }
