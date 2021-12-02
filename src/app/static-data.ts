8c8141362366fbfbc39e7d20dd0dca5a68d828ad
Sorokin_Maksim <askonev007@gmail.com>
2021-12-02 14:51:51 +0300

    Add user Nikolay Lambrov

--- src/app/static-data.ts
+++ src/app/static-data.ts
@@ -1,7 +1,7 @@
 export class StaticData {
 
   public static readonly BUG_WITH_ATTACHMENTS = 45649;
-  public static readonly COMMENT_WITH_USER_DATA = 208047;
+  public static readonly COMMENT_WITH_USER_DATA = 208589;
   public static readonly BUGZILLA_LINK = 'https://bugzilla.onlyoffice.com';
 
   public static readonly PRODUCTS =
