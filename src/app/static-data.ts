f80d6da981e2691866740c5df80bf886c3bf0986
Vitaliy Serov <154601125+VitaliySerov@users.noreply.github.com>
2024-07-12 20:11:33 +0300

    Update user list (#1482)

--- src/app/static-data.ts
+++ src/app/static-data.ts
@@ -1,7 +1,7 @@
 export class StaticData {
 
   public static readonly BUG_WITH_ATTACHMENTS = 45649;
-  public static readonly COMMENT_WITH_USER_DATA = 263681;
+  public static readonly COMMENT_WITH_USER_DATA = 275668;
   public static readonly BUGZILLA_LINK = 'https://bugzilla.onlyoffice.com';
 
   public static readonly PRODUCTS =
